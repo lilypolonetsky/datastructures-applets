d00adf99e2cfd4c1e4ddf3e5c91ed0d0786a0003
Ilana Radinsky <ilanaradinsky@gmail.com>
2018-03-21 21:49:42 -0400

    Update Array to contain Element records, reorganize how objects are displayed

--- PythonVisualizations/Array.py
+++ PythonVisualizations/Array.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import time
 import random
-from collections import namedtuple
+from recordclass import recordclass
 
 WIDTH = 800
 HEIGHT = 600
@@ -18,20 +18,33 @@ canvas = Canvas(frame, width=WIDTH, height=HEIGHT)
 window.title("Array")
 canvas.pack()
 
+global cleanup
+cleanup = []
+
+def clean():
+    global cleanup
+    if len(cleanup) > 0:
+        for o in cleanup:
+            canvas.delete(o)
+        window.update()
+
 # Button functions
+# Call clean() as the first line of each button function!
 def clickFind():
-    entered_text=textBox.get()
+    clean()
+    entered_text = textBox.get()
     txt = ''
-    if array.find(int(entered_text)):
+    result = array.find(int(entered_text))
+    if result:
         txt = "Found!"
     else:
         txt = "Value not found"
     outputText.set(txt)
 
+
 def close_window():
     window.destroy()
     exit()
-
 
 bottomframe = Frame(window)
 bottomframe.pack( side = BOTTOM )
@@ -50,19 +63,18 @@ output.grid(row=0, column=3, sticky=E)
 # exit button
 Button(bottomframe, text="EXIT", width=4, command=close_window).grid(row=2, column=0, sticky=W)
 
-Element = namedtuple('Element', ['val', 'color', 'display_shape', 'display_val'])
-Element.__new__.__defaults__ = (None,) * len(Element._fields)
+
 
 class Array(object):
+    Element = recordclass('Element', ['val', 'color', 'display_shape', 'display_val'])
+    Element.__new__.__defaults__ = (None,) * len(Element._fields)
+
     colors = ['red', 'green', 'blue', 'orange', 'yellow', 'cyan', 'magenta',
               'dodgerblue', 'turquoise', 'grey', 'gold', 'pink']
     nextColor = 0
 
-    def __init__(self):
-        self.list = []
-        #self.arrow = None
-        #self.foundBox = None
-        self.drawnObjects = []
+    def __init__(self, size=0):
+        self.list = [0]*size
 
     def get(self, index):
         try:
@@ -72,58 +84,105 @@ class Array(object):
             return -1
 
     def set(self, index, val):
-        self.list[index] = (val, Array.colors[Array.nextColor])
-        Array.nextColor = (Array.nextColor + 1)%len(Array.colors)
+        # reset the value of the Element at that index to val
+        self.list[index].val = val
+
+        # get the position of the displayed value
+        pos = canvas.coords(self.list[index].display_val)
+
+        # delete the displayed value and replace it with the updated value
+        canvas.delete(self.list[index].display_val)
+        self.list[index].display_val = canvas.create_text(pos[0], pos[1], text=str(val), font=('Helvetica', '20'))
+
+        # update window
+        window.update()
 
     def append(self, val):
-        self.list.append((val, Array.colors[Array.nextColor]))
+        # create new cell and cell value display objects
+        cell = canvas.create_rectangle(ARRAY_X0+CELL_SIZE*len(self.list), ARRAY_Y0, ARRAY_X0+CELL_SIZE*(len(self.list)+1), ARRAY_Y0 + CELL_SIZE, fill=Array.colors[Array.nextColor])
+        cell_val = canvas.create_text(ARRAY_X0+CELL_SIZE*len(self.list) + (CELL_SIZE / 2), ARRAY_Y0 + (CELL_SIZE / 2), text=val,
+                                      font=('Helvetica', '20'))
+
+        # add a new Element to the list with the new value, color, and display objects
+        self.list.append(Array.Element(val, Array.colors[Array.nextColor], cell, cell_val))
+
+        # increment nextColor
         Array.nextColor = (Array.nextColor + 1) % len(Array.colors)
-        self.display()
+
+        # update window
+        window.update()
 
     def removeFromEnd(self):
-        self.list.pop()
-        self.display()
+        # pop an Element from the list
+        n = self.list.pop()
 
-    def clear(self):
-        for o in self.drawnObjects:
-            canvas.delete(o)
+        # delete the associated display objects
+        canvas.delete(n.display_shape)
+        canvas.delete(n.display_val)
+
+        # update window
+        window.update()
 
     def display(self):
-        self.clear()
+        canvas.delete("all")
         xpos = ARRAY_X0
         ypos = ARRAY_Y0
+
+        # go through each Element in the list
         for n in self.list:
             print(n)
+            # create display objects for the associated Elements
             cell = canvas.create_rectangle(xpos, ypos, xpos+CELL_SIZE, ypos+CELL_SIZE, fill=n[1])
             cell_val = canvas.create_text(xpos+(CELL_SIZE/2), ypos+(CELL_SIZE/2), text=n[0], font=('Helvetica', '20'))
-            self.drawnObjects.append(cell)
-            self.drawnObjects.append(cell_val)
-            xpos += 50
+
+            # save the display objects to the appropriate attributes of the Element object
+            n.display_shape = cell
+            n.display_val = cell_val
+
+            # increment xpos
+            xpos += CELL_SIZE
+
         window.update()
 
     def find(self, val):
-        self.clear()
+        global cleanup
+        findDisplayObjects = []
+        #canvas.delete(findDisplayObjects)
         self.display()
-        # Maybe, instead of having the foundBox, change color of value to green2?
+
+        # draw an arrow over the first cell
         x = ARRAY_X0 + (CELL_SIZE/2)
         y0 = ARRAY_Y0 - 40
         y1 = ARRAY_Y0 - 15
         arrow = canvas.create_line(x, y0, x, y1, arrow="last", fill='red')
-        self.drawnObjects.append(arrow)
+        findDisplayObjects.append(arrow)
+
+        # go through each Element in the list
         for n in self.list:
             window.update()
-            if n[0] == val:
-                pos = canvas.coords(arrow)
-                #foundBox = canvas.create_rectangle(pos[0]-(x-CELL_SIZE), ARRAY_Y0, pos[2]+(x-CELL_SIZE), ARRAY_Y0+CELL_SIZE, fill='', outline="green2", width="5")
-                #self.drawnObjects.append(foundBox)
-                cell_val = canvas.create_text(pos[0]-(x-CELL_SIZE) + (CELL_SIZE / 2), ARRAY_Y0 + (CELL_SIZE / 2), text=n[0],
-                                              font=('Helvetica', '25'), fill='green2')
-                self.drawnObjects.append(cell_val)
+
+            # if the value is found
+            if n.val == val:
+                # get the position of the displayed value
+                pos = canvas.coords(n.display_val)
+
+                # cover the current display value with the updated value in green
+                cell_val = canvas.create_text(pos[0], pos[1], text=str(val), font=('Helvetica', '22'), fill='green2')
+
+                # add the green value to findDisplayObjects for cleanup later
+                findDisplayObjects.append(cell_val)
+
+                # update the display
+                window.update()
+
+                cleanup += findDisplayObjects
                 return True
 
+            # if the value hasn't been found, wait 1 second, and then move the arrow over one cell
             time.sleep(1)
             canvas.move(arrow, CELL_SIZE, 0)
 
+        cleanup += findDisplayObjects
         return False
 
     def remove(self, index):
@@ -164,21 +223,39 @@ class Ball:
             self.xspeed = -self.xspeed
 
 
-
+#cleanup = []
 array = Array()
+array.display()
+
 for i in range(10):
     array.append(i)
 
-array.display()
+#array.display()
 
 time.sleep(1)
 
-#array.find(6)
-#time.sleep(1)
+array.find(3)
+time.sleep(1)
+
+clean()
+
 array.append(10)
 time.sleep(1)
+
+clean()
+
 array.remove(3)
+time.sleep(1)
+
+clean()
+
 array.append(3)
+time.sleep(1)
+
+clean()
+
+array.set(3, 20)
+time.sleep(1)
 
 window.mainloop()
 
