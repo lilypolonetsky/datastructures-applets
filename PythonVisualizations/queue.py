dfa965d4d05226c7c4976436a474d1abcc6c36e1
Sarah Engel <sarahadenae@gmail.com>
2019-02-14 17:27:38 -0500

    Added more comments to queue

--- PythonVisualizations/queue.py
+++ PythonVisualizations/queue.py
@@ -4,3 +4,10 @@
 # - grid of ten elements with empty spaces.
 #     - five white blocks
 #     - never redrawn or erased, when adding, just overlaying 
+
+# - Reorienting stack
+# - changing the motion
+# - adding button
+#     - toggles between allowable motions
+#     - with your onClick()
+# - changing how cells are added or popped
