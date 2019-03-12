df7b0490d65bd62bbbbc184c05e3baf24d8369a2
rleiser <44149699+rleiser@users.noreply.github.com>
2019-03-11 23:25:04 -0400

    Update Heap.py

--- PythonVisualizations/Heap.py
+++ PythonVisualizations/Heap.py
@@ -14,7 +14,8 @@ class Heap(object):
     def __init__(self, size):
         self.__arr = [None] * size
         self.__nElems = 0 
-        self.__ovals = []
+        self.__ovals = []  
+        self.__oElems = 0
         self.__nums = []
         self.__arrows = []
         
@@ -52,6 +53,8 @@ class Heap(object):
             root.update() 
         self.__ovals[cur], self.__ovals[other] = self.__ovals[other], self.__ovals[cur]
         self.__nums[cur], self.__nums[other] = self.__nums[other], self.__nums[cur]
+        
+        return True
      
      
 
@@ -60,30 +63,61 @@ class Heap(object):
         # Place new node at end of heap & trickle it up
 
         try: 
+            #print("this is self.__arr before insert", self.__arr)
             self.__arr[self.__nElems] = Node(k, d)
+            #print("this is self.__arr after inserting:", self.__arr)
             self.__nElems += 1
+            print("this is self.__nElems after instering now", self.__nElems)
         
             positionX = coordinates[0][self.__nElems-1]
             positionY = coordinates[1][self.__nElems-1]
             oval = w.create_oval(positionX, positionY, positionX+50, positionY+50, fill='yellow')
             text = w.create_text(positionX+25, positionY+25, font=('calibri', 12), text=str(self.__arr[self.__nElems-1].key))
             # list of each oval to access later on for swapping
-            self.__ovals.append(oval)
-            self.__nums.append(text)
-            parent = (self.__nElems-2)//2
-            if self.__nElems != 1:
-                arrow = w.create_line(coordinates[0][parent]+25, coordinates[1][parent]+50, positionX+20, positionY, arrow=tk.LAST)    
-                self.__arrows.append(arrow)
+            
+            ## Goal here is to insert new node into self.__ovals list, replace the 
+            ## last node which should be a None.
+            ## of course, if this list is empty, or doesn't have a None, then we just append a new oval
+            ## to the end of the list.
+            ## this is to take care of then case where you re-insert an element after removing an element
+            
+            ## same thing with __nums and __arrows
+            parent = (self.__nElems-2)//2            
             
             
-            return oval, text
+            if len(self.__ovals) != 0 and self.__ovals[self.__oElems-1]==None:
+                #print("this is oval list: ", self.__ovals)
+                #print("this is oval at last element, should be None:", self.__ovals[self.__nElems-1])
+        
+                self.__ovals[self.__nElems-1] = oval 
+                #print("this is ovals with the none replaced with the new node", self.__ovals)
+                self.__nums[self.__nElems-1] = text
+                arrow = w.create_line(coordinates[0][parent]+25, coordinates[1][parent]+50, positionX+20, positionY, arrow=tk.LAST) 
+                self.__arrows[self.__nElems-1] = arrow
+    
+                             
+                    
+            else:
+                #print("this node should be appended to end of list")
+                self.__ovals.append(oval)
+                self.__oElems+=1
+                #print("new ovals after appending to end:", self.__ovals)
+                #print("new length of ovals after appending to end", self.__oElems)
+                self.__nums.append(text)
+                if self.__nElems != 1:
+                    arrow = w.create_line(coordinates[0][parent]+25, coordinates[1][parent]+50, positionX+20, positionY, arrow=tk.LAST) 
+                    self.__arrows.append(arrow)
+                
+            
+
+            return True
         except:
             pass
   
             
-    def trickleUp(self, cur):
+    def trickleUp(self):
         cur = self.__nElems-1
-        bottom = self.__arr[self.__nElems-1] # the recently inserted Node
+        bottom = self.__arr[cur] # the recently inserted Node
         parent = (cur-1) // 2    # its parent's location
         # While cur hasn't reached the root, and cur's parent's
         # key is smaller than the new Node's key
@@ -103,6 +137,7 @@ class Heap(object):
         # root, or cur's parent's key is >= the new node's key. 
         # so place the new node into the current spot on the tree.
         self.__arr[cur] = bottom
+        return True
     
     
     
@@ -110,14 +145,17 @@ class Heap(object):
     def remove(self): 
         if self.__nElems == 0: return None, None # empty heap
 
-        # the answer will be the key/data from the root node
+        # save the root position
         root = self.__arr[0]
+        # decrease number of elements in arr since we will be removing the root
+        
         self.__nElems -= 1
 
         
         # Now place the last Node in the heap into the 
-        # root location, and trickle it down
+        # root location
         self.__arr[0] = self.__arr[self.__nElems]        
+        self.__arr[self.__nElems] = None
         
         # first swap the root
         self.swapRoot()
@@ -138,10 +176,12 @@ class Heap(object):
         w.delete(self.__ovals[0])
         w.delete(self.__nums[0])        
         # remove the last arrow
+        
+        print("this is nElems before we delete an arrow", self.__nElems)
         w.delete(self.__arrows[self.__nElems-1])
         self.__arrows[self.__nElems-1] = None
-        self.__ovals[0] = self.__ovals[self.__nElems]
-        self.__nums[0] = self.__nums[self.__nElems]
+        
+        
         
         increment = 100
         # swap the root node with the last node
@@ -150,11 +190,21 @@ class Heap(object):
             w.move(self.__nums[self.__nElems], changeX/increment, changeY/increment)
             root.update()
         
+        #print("this is oval after delete root", self.__ovals)
         
+        # put last element in root position
+        self.__ovals[0] = self.__ovals[self.__nElems]
+        self.__nums[0] = self.__nums[self.__nElems]
         # remove the last node from the oval list
         self.__ovals[self.__nElems] = None
-        self.__nums[self.__nElems] = None          
+        self.__nums[self.__nElems] = None 
+        
+        #self.__oElems-=1
+        print("this is oval after swapping last node with root and removing root:", self.__ovals)
+        print("this is length of oval after removing root and swap:", self.__oElems)
         root.update()    
+        
+        return True
     
     
     # this heap is now a min-heap       
@@ -199,6 +249,8 @@ class Heap(object):
         # is larger than both children. So place the old root node there
         
         self.__arr[cur] = top  # move original root to its correct position 
+        
+        return True
     
     #test that this heap follows min-heap conditions
     def isHeap(self, cur = 0):
@@ -251,11 +303,11 @@ coordinates = [ [375, 175, 575, 75, 275, 475, 675, 25, 125, 225, 325, 425, 525, 
 
 h = Heap(15)
 
-
+# this is the command called by the button to insert a node
 def insert_node():
     
-    num = h.insert(random.randint(0, 100), chr(ord('A') + 1))
-    h.trickleUp(num[0])
+    h.insert(random.randint(0, 100), chr(ord('A') + 1))
+    h.trickleUp()
 
 
 def remove_node():
