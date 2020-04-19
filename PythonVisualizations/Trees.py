fa43a0136d63f14dc642b5e9a9b146b52fff851b
Lily Polonetsky <lily.polonetsky@gmail.com>
2020-04-19 18:47:00 -0400

    delete and fill method

--- PythonVisualizations/Trees.py
+++ PythonVisualizations/Trees.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import time
+import time, random
 
 WIDTH = 1000
 HEIGHT = 400
@@ -47,16 +47,28 @@ class Tree(object):
 
     #Fill the tree with n nodes
     def fill(self, n):
-        pass
+        #delete any existing nodes by looping through the tree
+        canvas.delete("all")
+        self.__root = None
+        self.nElems = 0
+
+        #randomly generate a tree
+        nums = list(range(1, 99))
+        random.shuffle(nums)
+        while self.nElems < n and nums:
+            num = nums.pop()
+            self.insertElem(nums.pop(), False)
+
 
     # find node with given key k
     # return the associated data or None
-    def find(self, k):
+    def find(self, k, flag="normal"):
         global cleanup, running
         running = True
         findDisplayObjects = []
 
         # start at the root
+        level = 0
         cur = self.__root
         arrow = canvas.create_line(cur.coords[0], cur.coords[1] - CIRCLE_SIZE - ARROW_HEIGHT,
                                    cur.coords[0], cur.coords[1] - CIRCLE_SIZE, arrow="last", fill='red')
@@ -79,7 +91,8 @@ class Tree(object):
 
                 canvas.update()
                 cleanup += findDisplayObjects
-                return cur
+                if flag == "normal": return cur
+                else: return cur,level
 
             # go left ?
             if k < cur.key:
@@ -87,6 +100,7 @@ class Tree(object):
             # go right
             else:
                 cur = cur.rightChild
+            level += 1
 
         cleanup+=findDisplayObjects
         return None
@@ -184,8 +198,167 @@ class Tree(object):
             print(" " + str(cur), end="")
             self.inOrderTraversal(cur.rightChild)
 
-    def delete(self, key):
-        pass
+
+    def delete(self, k, animation = True):
+        global cleanup, running
+        running = True
+        findDisplayObjects = []
+
+        # start at the root
+        cur = self.__root
+        parent = self
+        direction = None
+        
+        #if animated, show the search for the item to be deleted
+        if animation:
+            arrow = canvas.create_line(cur.coords[0], cur.coords[1] - CIRCLE_SIZE - ARROW_HEIGHT,
+                                   cur.coords[0], cur.coords[1] - CIRCLE_SIZE, arrow="last", fill='red')
+            findDisplayObjects.append(arrow)
+            canvas.update()
+
+        # while we haven't found the key
+        while cur:
+            #if animated, show the search
+            if animation:
+                canvas.delete(arrow)
+                arrow = canvas.create_line(cur.coords[0], cur.coords[1] - CIRCLE_SIZE - ARROW_HEIGHT,
+                                       cur.coords[0], cur.coords[1] - CIRCLE_SIZE, arrow="last", fill='red')
+                findDisplayObjects.append(arrow)
+                canvas.update()
+                time.sleep(self.speed(1))
+
+            #found the item to be deleted
+            if cur.key == k:
+                #if animated, show that we found the item to be deleted
+                if animation:
+                    foundText = canvas.create_text(cur.coords[0], cur.coords[1], text=cur.key, font=VALUE_FONT,
+                                               fill="green")
+                    findDisplayObjects.append(foundText)
+                    canvas.update()
+                    cleanup += findDisplayObjects
+
+                    time.sleep(self.speed(1))
+                    canvas.delete(arrow)
+                    canvas.delete(foundText)
+                #go through the process of deleting the item
+                return self.__delete(parent, cur, direction)
+
+            parent = cur
+            # go left ?
+            if k < cur.key:
+                cur = cur.leftChild
+                direction = Child.LEFT
+            # go right
+            else:
+                cur = cur.rightChild
+                direction = Child.RIGHT
+
+        cleanup+=findDisplayObjects
+        return None
+        
+    def __delete(self, parent, cur, direction):
+        #save the deleted key
+        deleted = cur.key
+
+        #remove the drawing
+        canvas.delete(cur.id)
+        canvas.update()
+        time.sleep(self.speed(1))
+        
+        #determine the correct process for removing the node from the tree
+        if cur.leftChild:
+            if cur.rightChild:                  #case 3: left and right child exist
+                self.__promoteSuccessor(parent, cur, direction)
+            else:
+                if parent is self:              #no right child
+                    self.__root = cur.leftChild #root was deleted
+                    self.__reDraw(self.__root, None, None, 0)
+                elif parent.leftChild is cur:   #parent's left is cur, cur only has a left
+                    parent.leftChild = cur.leftChild
+                    self.__reDraw(parent.leftChild, parent, Child.LEFT)
+                else:                           #parent's right is cur, cur only has a left
+                    parent.rightChild = cur.leftChild
+                    self.__reDraw(parent.rightChild, parent, Child.RIGHT)
+        else:                                   #a right child exists or no children
+            if parent is self:                  #deleting the root
+                self.__root = cur.rightChild
+                self.__reDraw(self.__root, None, None, 0)
+            elif parent.leftChild is cur:       #parent's left is cur, cur only has a right child
+                parent.leftChild = cur.rightChild
+                self.__reDraw(parent.leftChild, parent, Child.LEFT)
+            else:                               #parent's right is cur, cur only has a right child
+                parent.rightChild = cur.rightChild
+                self.__reDraw(parent.rightChild, parent, Child.RIGHT)
+
+        self.nElems -= 1
+        return deleted
+
+    #cur is item that is being moved
+    #parent is the parent of cur
+    #childDirection is the direction that parent should point to cur
+    def __reDraw(self, cur, parent, childDirection, level="begin"):
+        if cur == None: return
+
+        # if the level of cur is unknown, find the level
+        if level == "begin": node, level = self.find(cur.key, flag="level")
+        #clean up the find arrow
+        cleanUp()
+
+        #remove the current node's representation from the canvas
+        canvas.delete(cur.id)
+        canvas.update()
+
+        #reDraw the item in the correct position
+        x,y = self.calculateCoordinates(parent, level, childDirection)
+        canvas.create_circle(x, y, CIRCLE_SIZE, tag = cur.id)
+        canvas.create_text(x,y, text=cur.key, font=VALUE_FONT, tag = cur.id)
+        if level !=0:
+            x1 = parent.coords[0]
+            y1 = parent.coords[1] +CIRCLE_SIZE
+            x2 = x
+            y2 = y-CIRCLE_SIZE
+            canvas.create_line(x1, y1, x2, y2, tag = cur.id)
+        cur.coords = (x,y)
+
+        #reDraw any of cur's children
+        self.__reDraw(cur.leftChild, cur, Child.LEFT, level+1)
+        self.__reDraw(cur.rightChild, cur, Child.RIGHT, level+1)
+
+    #description: method used when deleting a node with two children, locates the node to take the place of the deleted node and calls the appropiate methods
+    #             the node we want to "promote" is the key that is after the deleted key when all the keys are arranged in ascending order- this is the deleted node's right child's most left child
+    #parameters: parent is the parent of the node to be deleted, node is the node being deleted, direction indicates if node is the right or left child of its parent
+    def __promoteSuccessor(self, parent, node, direction):
+        global cleanup
+        findDisplayObjects = []
+
+        successor = node.rightChild
+        newParent = node
+
+        arrow = canvas.create_line(successor.coords[0], successor.coords[1] - CIRCLE_SIZE - ARROW_HEIGHT,
+                                       successor.coords[0], successor.coords[1] - CIRCLE_SIZE, arrow="last", fill='red')
+        findDisplayObjects.append(arrow)
+        canvas.update()
+        time.sleep(self.speed(1))
+                
+        #hunt for the right child's most left child
+        while successor.leftChild:
+            newParent = successor
+            successor = successor.leftChild
+
+            canvas.delete(arrow)
+            arrow = canvas.create_line(successor.coords[0], successor.coords[1] - CIRCLE_SIZE - ARROW_HEIGHT,
+                                       successor.coords[0], successor.coords[1] - CIRCLE_SIZE, arrow="last", fill='red')
+            findDisplayObjects.append(arrow)
+            canvas.update()
+            time.sleep(self.speed(1))
+
+        #move the correct key to the deleted node's slot
+        node.key = successor.key
+
+        cleanup += findDisplayObjects
+
+        self.__reDraw(node, parent, direction)
+        self.__delete(newParent, successor, direction)
 
     # ANIMATION METHODS
     def speed(self, sleepTime):
@@ -227,10 +400,25 @@ def clickFind():
     textBox.delete(0, END)
 
 def clickFill():
-    pass
+    entered_text = textBox.get()
+    if entered_text:
+        val = int(entered_text)
+        if val < 32:
+            tree.fill(val)
+        else:
+            outputText.set("Input value must be an integer from 0 to 31.")
+    textBox.delete(0, END)
 
 def clickDelete():
-    pass
+    entered_text = textBox.get()
+    if entered_text:
+        val = int(entered_text)
+        if val < 100:
+            deleted = tree.delete(int(entered_text))
+            outputText.set("Deleted!" if deleted else "Value not found")
+        else:
+            outputText.set("Input value must be an integer from 0 to 99.")
+    textBox.delete(0, END)
 
 #What can be inserted in the textbox
 def validate(action, index, value_if_allowed,
