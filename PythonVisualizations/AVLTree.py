3e56a2e9d31678921566ad9c805d6af509ec6662
Lily Polonetsky <36013651+lilypolonetsky@users.noreply.github.com>
2021-01-14 20:20:22 -0500

    Revert "Do not allow insertions past MAX_LEVEL"
    This reverts commit 2ea2ddf3f41a9ab3f2d698a2d2d79b4fc81be170.

--- PythonVisualizations/AVLTree.py
+++ PythonVisualizations/AVLTree.py
@@ -70,11 +70,6 @@ class AVLTree(BinaryTreeBase):
       
       # key already exists in tree
       if key == node.getKey():
-         self.setMessage(
-               "Error! Key" + str(key) + " already exists in the tree")
-         self.cleanUp(callEnviron)
-         return node, False
-
          self.cleanUp(callEnviron)
          return node, False
           
