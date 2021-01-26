728ef9e4d1bf8a88de3cc3fd137dc8afa4647831
John Canning <-h>
2021-01-26 12:24:09 -0800

    Combine imaage play controls into a single operations button space
    Add warning about missing Pillow package
    Add sample 1-argument operation button to help see target result better.

--- PythonVisualizations/Test Results/testImageButton.py
+++ PythonVisualizations/Test Results/testImageButton.py
@@ -1,14 +1,20 @@
-from PIL import Image as Img
-from PIL import ImageTk
 from tkinter import *
 from tkinter import ttk
 
 try:
-    from coordinates import *
-    from VisualizationApp import *
+   from PIL import Image as Img
+   from PIL import ImageTk
+except ModuleNotFoundError as e:
+   print('Pillow module not found.  Did you try running:')
+   print('pip3 install -r requirements.txt')
+   raise e
+
+try:
+   from coordinates import *
+   from VisualizationApp import *
 except ModuleNotFoundError:
-    from .coordinates import *
-    from .VisualizationApp import *
+   from .coordinates import *
+   from .VisualizationApp import *
 
 V = vector
 
@@ -18,36 +24,60 @@ class TestImageButton(VisualizationApp):
       self.testsRun = 0
       self.makeButtons()
 
-   def makeButtons(self):
+   def makeButtons(self, maxRows=4):
       self.testButton = self.addOperation("Disable", self.clickTest)
+      self.insertButton = self.addOperation(
+          "Insert", self.clickInsert, numArguments=1, argHelpText=['item'],
+          helpText='Insert item nowhere')
+      self.playControls = self.makePlayControls(maxRows=maxRows)
+
+   def clickInsert(self):
+      self.setMessage('Insert {} nowhere'.format(self.getArgument()))
+
+   def makePlayControls(self, maxRows=4):
       imageFiles = ('play-symbol.png', 'pause-symbol.png',
                     'skip-next-symbol.png', 'stop-symbol.png')
-      self.playImages = [None] * len(imageFiles)
+      self.images = [None] * len(imageFiles)
       self.imageBtns = [None] * len(imageFiles)
       rect_bg = self.canvas.create_rectangle(
           90, 90, 200, 110 + len(imageFiles) * 60, fil='beige', outline='',
           width=0)
-      row, column = 1, 0
+      
+      self.playControlsFrame = Frame(self.operations, bg=self.OPERATIONS_BG)
+      gridItems = gridDict(self.operations) # Operations inserted in grid
+      nColumns, nRows = self.operations.grid_size()
+      withArgument = [
+         gridItems[0, row] for row in range(nRows)
+         if isinstance(gridItems[0, row], self.buttonTypes)]
+      withoutArgument = [
+         gridItems[col, row]
+         for row in range(nRows) for col in range(4, nColumns)
+         if isinstance(gridItems[col, row], self.buttonTypes)]
+      buttonRow = len(withoutArgument) % maxRows + 1
+      self.playControlsFrame.grid(column=4 + len(withoutArgument) // maxRows,
+                                  row=buttonRow, sticky=(E,W))
+
+      row, column = 0, 0
       targetSize = (abs(self.CONTROLS_FONT[1]),) * 2
       setMsg = lambda m: lambda: self.setMessage(m)
       for i, imagefile in enumerate(imageFiles):
-          self.playImages[i] = Img.open(imagefile)
+          self.images[i] = Img.open(imagefile)
           imgItem = self.canvas.create_image(
-              (column + 1) * 100, row * 100, anchor=NW,
-              image=ImageTk.PhotoImage(self.playImages[i]))
-          dims = self.playImages[i].size
+              (column + 1) * 100, (row + 1) * 100, anchor=NW,
+              image=ImageTk.PhotoImage(self.images[i]))
+          dims = self.images[i].size
           ratios = V(targetSize) / V(dims)
           minRatio = min(*ratios)
-          self.playImages[i] = ImageTk.PhotoImage(self.playImages[i].resize(
+          self.images[i] = ImageTk.PhotoImage(self.images[i].resize(
               tuple(int(round(d * minRatio)) for d in dims)))
           Bclass = Button # ttk.Button if i % 2 == 0 else Button
-          btn = Bclass(self.operations, image=self.playImages[i])
-          btn.image = self.playImages[i]
-          btn.grid(row=row, column=column, padx=8, sticky=(E, W))
+          btn = Bclass(self.playControlsFrame, image=self.images[i])
+          btn.image = self.images[i]
+          btn.grid(row=row, column=column, padx=0, sticky=(E, W))
           self.imageBtns[i] = btn
           btn['command'] = self.runOperation(
-              setMsg('Image button {} pressed'.format(row)), True)
-          row += 1
+              setMsg('{} button pressed'.format(imagefile)), True)
+          column += 1
           setattr(btn, 'required_args', 0)
 
    def clickTest(self):
