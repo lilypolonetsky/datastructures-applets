80f4817e62cb575fb9c772057f25118ddb824ca6
John Canning <-h>
2021-01-27 12:21:27 -0800

    Try both Tk and Ttk Buttons and use getOperations()
    Changed output messages to show a difference when invoked by pressing
    space after shifting focus to a button.

--- PythonVisualizations/Test Results/testImageButton.py
+++ PythonVisualizations/Test Results/testImageButton.py
@@ -16,19 +16,24 @@ except ModuleNotFoundError:
    from .coordinates import *
    from .VisualizationApp import *
 
+from pprint import pprint
+
 V = vector
 
 class TestImageButton(VisualizationApp):
-   def __init__(self):
-      super().__init__()
+   def __init__(self, title='Test Image Button'):
+      super().__init__(title=title)
       self.testsRun = 0
       self.makeButtons()
 
    def makeButtons(self, maxRows=4):
-      self.testButton = self.addOperation("Disable", self.clickTest)
+      self.testButton = self.addOperation(
+         "Disable", self.clickTest, maxRows=maxRows)
+      self.nothingButton = self.addOperation(
+         "Do nothing", self.clickNothing, buttonType=Button, maxRows=maxRows)
       self.insertButton = self.addOperation(
           "Insert", self.clickInsert, numArguments=1, argHelpText=['item'],
-          helpText='Insert item nowhere')
+          helpText='Insert item nowhere', maxRows=maxRows)
       self.playControls = self.makePlayControls(maxRows=maxRows)
 
    def clickInsert(self):
@@ -43,23 +48,14 @@ class TestImageButton(VisualizationApp):
           90, 90, 200, 110 + len(imageFiles) * 60, fil='beige', outline='',
           width=0)
       
-      self.playControlsFrame = Frame(self.operations, bg=self.OPERATIONS_BG)
-      gridItems = gridDict(self.operations) # Operations inserted in grid
-      nColumns, nRows = self.operations.grid_size()
-      withArgument = [
-         gridItems[0, row] for row in range(nRows)
-         if isinstance(gridItems[0, row], self.buttonTypes)]
-      withoutArgument = [
-         gridItems[col, row]
-         for row in range(nRows) for col in range(4, nColumns)
-         if isinstance(gridItems[col, row], self.buttonTypes)]
-      buttonRow = len(withoutArgument) % maxRows + 1
-      self.playControlsFrame.grid(column=4 + len(withoutArgument) // maxRows,
+      self.controlsFrame = Frame(self.operations, bg=self.OPERATIONS_BG)
+      withArgs, withoutArgs, nColumns, nRows = self.getOperations()
+      buttonRow = len(withoutArgs) % maxRows + 1
+      self.controlsFrame.grid(column=4 + len(withoutArgs) // maxRows,
                                   row=buttonRow, sticky=(E,W))
 
       row, column = 0, 0
       targetSize = (abs(self.CONTROLS_FONT[1]),) * 2
-      setMsg = lambda m: lambda: self.setMessage(m)
       for i, imagefile in enumerate(imageFiles):
           self.images[i] = Img.open(imagefile)
           imgItem = self.canvas.create_image(
@@ -71,15 +67,29 @@ class TestImageButton(VisualizationApp):
           self.images[i] = ImageTk.PhotoImage(self.images[i].resize(
               tuple(int(round(d * minRatio)) for d in dims)))
           Bclass = Button # ttk.Button if i % 2 == 0 else Button
-          btn = Bclass(self.playControlsFrame, image=self.images[i])
+          btn = Bclass(self.controlsFrame, image=self.images[i])
           btn.image = self.images[i]
           btn.grid(row=row, column=column, padx=0, sticky=(E, W))
           self.imageBtns[i] = btn
           btn['command'] = self.runOperation(
-              setMsg('{} button pressed'.format(imagefile)), True)
+              self.setMsg('{} button pressed'.format(imagefile)), True)
           column += 1
           setattr(btn, 'required_args', 0)
 
+   def setMsg(self, msg):
+      count = 0
+      def handler(event=None):
+         nonlocal count
+         count += 1
+         self.setMessage(msg + '\n{} time{}'.format(
+            count, '' if count == 1 else 's'))
+      return handler
+
+   def clickNothing(self):
+      self.testsRun += 1
+      self.setMessage('After {} test{} run\ndo nothing'.format(
+         self.testsRun, '' if self.testsRun == 1 else 's'))
+      
    def clickTest(self):
       self.testsRun += 1
       self.setMessage('{} test{} run'.format(
